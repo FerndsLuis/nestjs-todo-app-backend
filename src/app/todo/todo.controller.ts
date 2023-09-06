f02964fc4b601bc2ff080be8ded5ca67f15426bc
Luis <hsantos.luis@outlook.com>
2023-09-05 21:43:49 -0300

    feat: added swagger in project and documentation

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,3 +1,4 @@
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto copy';
 import { TodoService } from './todo.service';
@@ -13,27 +14,80 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { type } from 'os';
+import { IndexTodoSwagger } from './swagger/index-todo.swagger';
+import { CreateTodoSwagger } from './swagger/create-todo.swagger';
+import { ShowTodoSwagger } from './swagger/show-todo.swagger';
+import { UpdateTodoDtoSwagger } from './swagger/update-todo.swagger';
+import { BadRequestSwagger } from 'src/helpers/swagger/bad-request.swagger';
+import { NotFoundSwagger } from 'src/helpers/swagger/not-found-swagger';
 
 @Controller('api/v1/todos')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos os TODOs' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de tarefas',
+    type: IndexTodoSwagger,
+    isArray: true,
+  })
   async index() {
     return this.todoService.findAll();
   }
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Nova tarefa criada com sucesso',
+    type: CreateTodoSwagger,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Parâmetros inválidos',
+    type: BadRequestSwagger,
+  })
+  @ApiOperation({ summary: 'Adicionar TODO' })
   async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Dados de uma tarefa retornado com sucesso',
+    type: ShowTodoSwagger,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não encontrada',
+    type: NotFoundSwagger,
+  })
+  @ApiOperation({ summary: 'Busca um TODO' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneByOrFail(id);
   }
 
   @Put(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Tarefa atualizada com sucesso',
+    type: UpdateTodoDtoSwagger,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não encontrada',
+    type: NotFoundSwagger,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Dados inválidos',
+    type: BadRequestSwagger,
+  })
+  @ApiOperation({ summary: 'Atualizar os dados de um TODO' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() body: UpdateTodoDto,
@@ -42,6 +96,13 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 204, description: 'Tarefa deletada com sucesso' })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não encontrada',
+    type: NotFoundSwagger,
+  })
+  @ApiOperation({ summary: 'Deletar um TODO' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.todoService.deleteById(id);
